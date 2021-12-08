eca71672c51d9d383081f59899da2e1fe19c5386
Grégory Dziedzic <gregory.dziedzic@gmail.com>
2021-12-08 11:16:00 +0100

    V7.0 Victoire détectée

--- actions.mjs
+++ actions.mjs
@@ -17,6 +17,12 @@ function isEmpty(object){
 
 function clickedCell(event) {
     event.target.classList.toggle('clicked');
+    if (mode_resolution) {
+        console.log("Avant appel")
+        if (exporterNono()) {
+            console.log("gagné!")
+        }
+    }
 }
 
 function displayJSON (object){
@@ -24,7 +30,6 @@ function displayJSON (object){
         document.querySelector('pre').remove();
     }
 
-    console.log(object)
     let pre = document.createElement("pre")
     pre.innerText = JSON.stringify(object)
     body.appendChild(pre)
@@ -53,14 +58,9 @@ async function nonogramPicker() {
         document.querySelectorAll('.titre_nono').forEach(item => {
             item.addEventListener('click', ev => {
                 nonogram_in_page = nonograms.filter(nonogram => nonogram.title===item.innerText)[0]
-                console.log("item.innerText: ", item.innerText)
-                console.log("nonograms: ", nonograms)
-                console.log("Test correspondance: ", nonograms[0].title===item.innerText)
-
-
-                console.log(nonogram_in_page)
                 displayJSON (nonogram_in_page)
                 createTable(nonogram_in_page)
+                console.log(nonogram_in_page)
             }
             )
           })
@@ -71,36 +71,19 @@ async function nonogramPicker() {
 
 }
 
-// function loadNonogram () {
-//     nonogram = 
-// }
-
-// async function nonogramPicker() {
-//     await displayAvailableNonograms()
-//     document.querySelectorAll('.titre_nono').forEach(item => {
-//         item.addEventListener('click',
-//           loadNonogram
-//         )
-//       })
- 
-// }
-
 function tabToJSON() {
     const tbody = document.getElementsByTagName("tbody")[0]
     const tab_array = Array.from(tbody.children);
-    console.log("tabarray: ", tab_array);
     json_table = {
         nom_du_nonogramme: form.titre.value,
         data:[]
     }
     tab_array.forEach((row,i) => {
         const row_array = Array.from(row.children)
-        console.log('row_array:', row_array)
         json_table.data.push(
             row_array.map(cell => cell.classList.contains("clicked"))
         )
     })
-    console.log("JSON: ", json_table);
     return json_table
 }
 
@@ -108,7 +91,6 @@ function exporterNono () {
     const json_table = tabToJSON()
     rows = []
     columns = []
-    console.log("json table: ", json_table)
     for (let i=0; i<json_table.data.length; i++){
         rows[i] = []
         columns[i] = []
@@ -141,7 +123,6 @@ function exporterNono () {
             }
         }
     }
-    console.log("Rows tips: ", rows)
 
     columns = []
     for (let i=0; i<json_table.data[0].length; i++){
@@ -169,11 +150,24 @@ function exporterNono () {
             }
         }
     }
-    console.log("Column tips: ", columns)
+    if (mode_resolution) {
+        // console.log("nonogram_in_page :", nonogram_in_page)
+        // console.log("rows: ", rows.slice(1))
+        // console.log("columns: ", columns.slice(1))
+        // console.log("nono_in_page_rows: ", nonogram_in_page.data.rows)
+        // console.log("nono_in_page_columns: ", nonogram_in_page.data.columns)
+        // if (JSON.stringify(nonogram_in_page.data.rows) === JSON.stringify(rows.slice(1))) {
+        //     console.log("Wesh rows")
+        // }
+        if ((JSON.stringify(nonogram_in_page.data.rows) === JSON.stringify(rows.slice(1))) && (JSON.stringify(nonogram_in_page.data.columns) === JSON.stringify(columns.slice(1)))) {
+            return true
+        } else {
+            return false
+        }
+    }
     nonogram_in_page["title"] = json_table.nom_du_nonogramme
     nonogram_in_page["data"] = {"rows":rows,"columns":columns}
     nonogram_in_page["size"] = {"rows":json_table.data.length, "columns":json_table.data[0].length}
-    console.log("nonogramme: ", nonogram_in_page)
     displayJSON (nonogram_in_page)
 }
   
@@ -181,10 +175,8 @@ function createTable(nonogram) {
     if (document.querySelector('table')){
         document.querySelector('table').remove();
     }
-    console.log(form.lignes.value)
     let table = document.createElement("table")
     let tbody = document.createElement("tbody")
-    console.log(nonogram)
 
     if (!(isEmpty(nonogram))) {
         mode_resolution = true
@@ -229,7 +221,6 @@ function createTable(nonogram) {
     body.appendChild(table)
     const cellules = document.querySelectorAll("td")
     cellules.forEach(cell => {
-        console.log(cell)
         cell.addEventListener("click", clickedCell)
     })
 }
